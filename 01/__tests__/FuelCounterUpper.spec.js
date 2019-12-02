9ae5b8bbf3b96b78eb43c7acaf7c817c17af0a0a
Seth Kellas <sjkellas@gmail.com>
2019-12-01 20:19:30 -0500

    Actual use case

--- 01/__tests__/FuelCounterUpper.spec.js
+++ 01/__tests__/FuelCounterUpper.spec.js
@@ -6,4 +6,10 @@ describe('Fuel Counter Upper', () => {
     it('will calculate for mass of 14', () => {
         expect(calculateFuelUsageByMass(14)).toEqual(2);
     });
+    it('will calculate for mass of 1969', () => {
+        expect(calculateFuelUsageByMass(1969)).toEqual(654);
+    });
+    it('will calculate for mass of 100756', () => {
+        expect(calculateFuelUsageByMass(100756)).toEqual(33583);
+    });
 });
---
+++ 01/index.js
@@ -0,0 +1,6 @@
+const calculateFuelUsageByMass = require('./src/FuelCounterUpper');
+const fs = require('fs');
+
+fs.readFile('./res/input.txt', 'utf8', (err, contents) => {
+    console.log(contents.split("\n").map(val => calculateFuelUsageByMass(val)).reduce((a,b) => a + b));
+});
