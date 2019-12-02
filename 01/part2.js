499d9c3c555e102d2008f1931707676c3aa1b005
Seth Kellas <sjkellas@gmail.com>
2019-12-01 20:56:50 -0500

    Adding part2 completion

--- 01/part2.js
+++ 01/part2.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile('./res/input.txt', 'utf8', (err, contents) => {
     console.log(contents.split("\n")
-            .map(val => FuelCounterUpper.calculateFuelUsageByMass(val))
+            .map(val => FuelCounterUpper.calculateTrueFuelUsageByMass(val))
             .reduce((a,b) => a + b, 0 )
         );
 });
