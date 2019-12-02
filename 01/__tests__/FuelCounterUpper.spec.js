2932d090273a952eb587faf26724886f9c64eddc
Seth Kellas <sjkellas@gmail.com>
2019-12-01 20:37:45 -0500

    Part 2, refactor exports

--- 01/__tests__/FuelCounterUpper.spec.js
+++ 01/__tests__/FuelCounterUpper.spec.js
@@ -1,24 +1,27 @@
-const calculateFuelUsageByMass = require('../src/FuelCounterUpper');
+const FuelUsageByMass = require('../src/FuelCounterUpper');
 describe('Fuel Counter Upper', () => {
     it('will calculate for mass of 12', () => {
-        expect(calculateFuelUsageByMass(12)).toEqual(2);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(12)).toEqual(2);
     });
     it('will calculate for mass of 14', () => {
-        expect(calculateFuelUsageByMass(14)).toEqual(2);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(14)).toEqual(2);
     });
     it('will calculate for mass of 1969', () => {
-        expect(calculateFuelUsageByMass(1969)).toEqual(654);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(1969)).toEqual(654);
     });
     it('will calculate for mass of 100756', () => {
-        expect(calculateFuelUsageByMass(100756)).toEqual(33583);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(100756)).toEqual(33583);
     });
     it('will calculate for mass of 50572', () => {
-        expect(calculateFuelUsageByMass(50572)).toEqual(16855);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(50572)).toEqual(16855);
     });
     it('will calculate for mass of 98599', () => {
-        expect(calculateFuelUsageByMass(98599)).toEqual(32864);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(98599)).toEqual(32864);
     });
     it('will calculate for mass of 1', () => {
-        expect(calculateFuelUsageByMass(1)).toEqual(0);
+        expect(FuelUsageByMass.calculateFuelUsageByMass(1)).toEqual(0);
+    });
+    it('will calculate true value for mass of 12', () => {
+        expect(FuelUsageByMass.calculateTrueFuelUsageByMass(12)).toEqual(2);
     });
 });
--- 01/src/FuelCounterUpper.js
+++ 01/src/FuelCounterUpper.js
@@ -1,3 +1,8 @@
-module.exports = function calculateFuelUsageByMass(mass) {
-    return mass > 2 ? Math.floor(mass / 3) - 2 : 0;
+module.exports = {
+    calculateFuelUsageByMass: function calculateFuelUsageByMass(mass) {
+        return mass > 2 ? Math.floor(mass / 3) - 2 : 0;
+    },
+    calculateTrueFuelUsageByMass: function calculateTrueFuelUsageByMass(mass) {
+        return 2;
+    }
 };
